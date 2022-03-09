b6a249c17bad07b4b010f13a359c254411d821e4
xiaokeqi <lidancode@163.com>
2022-03-09 16:16:52 +0800

    命令配置

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "CopyCode" is now active!');
+	let currentEditor: any = vscode.window.activeTextEditor
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -19,6 +20,28 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from CopyCode!');
 	});
 
+	let newJsTemplate = vscode.commands.registerCommand('CopyCode.newJsFile', () => {
+		vscode.window.showInformationMessage('newJsFile from CopyCode!');
+	});
+
+	let newCssTemplate = vscode.commands.registerCommand('CopyCode.newCssFile', () => {
+		vscode.window.showInformationMessage('newCssFile from CopyCode!');
+	});
+	let newCssSnipcode = vscode.commands.registerCommand('CopyCode.newCssSnipcode', (textEditor, edit) => {
+		// console.log(textEditor.document)
+		console.log(currentEditor.document.getText(new vscode.Range(currentEditor.selection.start, currentEditor.selection.end)))
+		// console.log(textEditor.document.getText()
+		// vscode.window.showInformationMessage(currentEditor.selection)
+		vscode.window.showInformationMessage('newCssFile from CopyCode!');
+	});
+	let newJsSnipcode = vscode.commands.registerCommand('CopyCode.newJsSnipcode', () => {
+		// vscode.window.showInformationMessage(currentEditor.selection)
+		vscode.window.showInformationMessage('newCssFile from CopyCode!');
+	});
+	context.subscriptions.push(newCssSnipcode);
+	context.subscriptions.push(newJsSnipcode);
+	context.subscriptions.push(newCssTemplate);
+	context.subscriptions.push(newJsTemplate);
 	context.subscriptions.push(disposable);
 }
 
