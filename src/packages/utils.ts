483e783cd43c38c76946076473bda1d1d02dbe67
xiaokeqi <lidancode@163.com>
2022-03-10 14:48:16 +0800

    新建模板成功

--- src/packages/utils.ts
+++ src/packages/utils.ts
@@ -9,30 +9,43 @@ const config = require('./config.json')
  * @returns 
  */
 export function newTemplate(filename: string, source:string) {
-  const extensionPlugin = vscode.extensions.getExtension(config.extensionPath)
-  if(!extensionPlugin) {
-    vscode.window.showErrorMessage('您没有安装CopyPaste,请去应用商店安装')
-    return
-  }
-  const { extensionPath } = extensionPlugin
-  const templateDirPath = `${extensionPath}/.copypaste`
+  const templateDirPath:string = getInstalledExtensionPath()
   if(!isExist(templateDirPath)) {
     cp.execSync(`mkdir -p ${templateDirPath}`)
   }
   const ext = path.extname(source)
   let sourceContent = fs.readFileSync(source)
   // 若直接在fs中使用模板字符串变量赋值，创建不到.copypaste文件夹中
-  const templatePath = `${templateDirPath}/${filename}.${ext}`
-  if(isExist(templateDirPath)) {
+  const templatePath = `${templateDirPath}/${filename}${ext}`
+  if(isExist(templatePath)) {
     vscode.window.showWarningMessage('已存在同名模板，是否覆盖', { modal: true }, 'yes').then(pick =>{
       if (pick === 'yes') {
-        fs.writeFileSync(templatePath, sourceContent, 'utf-8')
-        vscode.window.showInformationMessage("Success")
+        newSuccess()
        } else {
           vscode.window.showInformationMessage("已存在同名模板,换一个试试？");
        }
     });
+  } else {
+    newSuccess()
   }
+
+  function newSuccess() {
+    fs.writeFileSync(templatePath, sourceContent, 'utf-8')
+    dbTransaction('add', `${filename}${ext}`)
+    vscode.window.showInformationMessage("Success")
+  }
+
+}
+
+export function getInstalledExtensionPath():string {
+  const extensionPlugin = vscode.extensions.getExtension(config.extensionPath)
+  if(!extensionPlugin) {
+    vscode.window.showErrorMessage('您没有安装CopyPaste,请去应用商店安装')
+    return ''
+  }
+  const { extensionPath } = extensionPlugin
+  const templateDirPath = `${path.dirname(extensionPath)}/${config.cacheDir}`
+  return templateDirPath
 }
 
 export function isExist(path: string) {
@@ -44,11 +57,43 @@ export function isExist(path: string) {
   }
 }
 /**
- * 
- * @param path 充当db的文件地址
- * @param type 操作类型，如增加：add,修改：update, 删除：del, 移动：move，查看：get
+ * @param type 操作类型，
+ * 如增加：add,
+ * 修改：update, 
+ * 删除：del, 
+ * 移动：move，
+ * 查看某个：get
+ * 查看所有：all
  * @param key 操作某个文件
  */
-export function dbTransaction(path: string, type: string, key: string) {
+export function dbTransaction(type: string, key: string) {
+  const templateDirPath:string = getInstalledExtensionPath()
+  const dbPath = `${templateDirPath}/${config.dbFile}`
+  let dbObj: any = {}
+  if(isExist(dbPath)) {
+    dbObj = JSON.parse(require(dbPath))
+  }
+  switch(type) {
+    // 新增模板
+    case 'add':
+      if(!dbObj[key]) {
+        dbObj[key] = {
+          name: key,
+          path: `${templateDirPath}/${key}`,
+          relate: '',
+          count: 0
+        }
+      }
+      break;
+    case 'update':
+      break;
+    case 'del':
+      break;
+    case 'move':
+      break;
+    case 'get':
+      break;
+  }
+  fs.writeFileSync(dbPath, JSON.stringify(dbObj, null, 4), 'utf-8')
 
 }
