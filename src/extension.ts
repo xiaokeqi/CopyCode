62bd97f50671a8f79c59a90ffe940a4a9bce03ce
xiaokeqi <lidancode@163.com>
2022-03-24 18:41:16 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import newJsFile from './packages/saveJsFile';
-import newCssFile from './packages/saveCssFile';
-import newSnippet from './packages/saveSnippet';
-import useTemplate from './packages/useTemplate';
-import useSnippetTemp from './packages/useSnipcodeTemplate'
+import newJsFile from './components/saveJsFile';
+import newCssFile from './components/saveCssFile';
+import newSnippet from './components/saveSnippet';
+import useTemplate from './components/useTemplate';
+import useSnippetTemp from './components/useSnipcodeTemplate'
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,7 +13,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "CopyPaste" is now active!');
-	let currentEditor: any = vscode.window.activeTextEditor
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -40,7 +39,11 @@ export function activate(context: vscode.ExtensionContext) {
 		useSnippetTemp()
 	})
 
+	let showManagePage = vscode.commands.registerCommand('CopyPaste.templateManage', () => {
+		console.log('后台管理')
+	})
 
+	context.subscriptions.push(showManagePage);
 	context.subscriptions.push(useSnipcode)
 	context.subscriptions.push(useCssTemplate);
 	context.subscriptions.push(useJsTemplate);
